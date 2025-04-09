c6ed0b4c16bbec251aefa2a0fcc0c3ac50fcbf0f
landmines <bbafowler@gmail.com>
2025-04-09 17:13:13 +0000

    update test functionality

--- agent_runner.py
+++ agent_runner.py
@@ -1,4 +1,3 @@
-# agent_runner.py
 import os
 import re
 import json
@@ -149,6 +148,7 @@ def dispatch_intent(intent, task, data):
     match_create = re.search(r"create (?:a )?file named ['\"]?([\w\-.]+)['\"]?", task_lower)
     match_append = re.search(r"append .* to ['\"]?([\w\-.]+)['\"]?", task_lower)
     match_edit = re.search(r"replace .* in ['\"]?([\w\-.]+)['\"]?", task_lower)
+    match_delete = re.search(r"(?:delete|remove)(?: the)? file ['\"]?([\w\-.]+)['\"]?", task_lower)
 
     if match_create:
         filename = match_create.group(1)
@@ -175,6 +175,14 @@ def dispatch_intent(intent, task, data):
             "filename": filename,
             "instructions": data.get("instructions", task),
             "notes": "Smartly inferred: edit_file"
+        }, fallback_used
+
+    if match_delete:
+        filename = match_delete.group(1)
+        return {
+            "action": "delete_file",
+            "filename": filename,
+            "notes": "Smartly inferred: delete_file"
         }, fallback_used
 
     if "summarize" in task_lower or "list" in task_lower:
